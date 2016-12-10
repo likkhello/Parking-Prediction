81871278b40d4a1146945a3eddacfcc9cc085267
likkhello <kklihello@gmail.com>
2016-12-11 00:09:40 +0800

    Check in
    Parking Prediction

---
+++ README.md
@@ -0,0 +1,60 @@
+---
+title: "README_parking_prediction"
+author: "likk"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Introduction
+The following project challenge is provided by Smarking. Smarking is a driven group of MIT PhDs, engineers, and business professionals who set out on a journey to bring cutting edge solutions to the parking world. In our current 11-person team, there are 5 people from MIT, 6 with PhD background, and two serial entrepreneurs. In the past year, our business grew at an average pace of over 40% month-over-month, and we are post-revenue.
+
+This challenge is part of the application for their software engineering position. You are also welcome to complete it for your own practice, it's a great analytics exercise.
+
+## Project Description
+The given data set (transations.csv) consists of more than three years of parking transaction data (from Jan 1, 2013 to Jan 31, 2016). There are two columns: entry time and exit time. Each row represents one transaction. Times are represented in ISO format.
+
+Data Set: https://www.dropbox.com/s/g0ucylnjz375nhv/trans.csv?dl=0
+
+For example:
+```{r, eval=FALSE, message=FALSE}
+entry time, exit time
+2015-01-01 01:32:25, 2015-01-01 03:02:52
+2015-01-01 01:58:11, 2015-01-01 06:39:03
+```
+We would like to provide one-month occupancy (number of cars in the garage) prediction to our clients. To be more specific, we would like to provide estimated occupancy for each of the following hours:
+```{r, eval=FALSE, message=FALSE}
+[2016-02-01 00:00:00, 2016-02-01 01:00:00, ..., 2016-02-29 23:00:00]
+```
+Requirements:
+
+Provide hourly predicted occupancy numbers for all the hours above in a csv file.
+Provide an interactive prediction chart on a web page. It should at least allow users to hover and see the number of each data point.
+Provide an interface to allow users to upload csv files with actual occupancy numbers. Visualize the actual numbers with the predicted numbers in the same chart after the upload completes. The csv file is in the following format:
+```{r, eval=FALSE, message=FALSE}
+ hour, occupancy
+ 2016-02-01 00:00:00, 100
+ 2016-02-01 01:00:00, 125
+...
+```
+ If actual occupancy numbers are provided, calculate the accuracy of your prediction and show the number on the web page. The measurement could be of your choice. Explain why you make that choice.
+ 
+## Result Demonstration
+ Step1: Transform raw entry and exit time data into hourly occupancy data
+ Step2: Develop linear regression model using above data and improved model accuracy to above 0.9 of R-square value by tuning hyper parameters
+ Step3: Setup Website using Flask as backend and bootstrap as frontend to provide interactive hourly occupancy prediction
+ Step4: Add daily parking calendar, monthly heatmap, daily linechart, prediction chart and actual occupancy chart.
+ 
+### Daily Parking Calendar
+ <img src=“http://ogx7uv5qv.bkt.clouddn.com/calendar.png”>
+### Monthly Heatmap
+<img src=“http://ogx7uv5qv.bkt.clouddn.com/heatmap.png”>
+### Daily Linechart
+<img src=“http://ogx7uv5qv.bkt.clouddn.com/linechart.png”>
+### Prediction Chart
+<img src=“http://ogx7uv5qv.bkt.clouddn.com/prediction.png”>
+### Actual Occupancy Chart
+<img src=“http://ogx7uv5qv.bkt.clouddn.com/actual_compare2.png”>
+ 
---
+++ webapp/app.py
@@ -0,0 +1,145 @@
+import os
+from flask import Flask, render_template, jsonify, redirect, url_for, request, flash
+from datetime import datetime, date, timedelta
+from sklearn.metrics import r2_score, mean_squared_error
+# import calendar
+import json
+import csv
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+# ALLOWED_EXTENSIONS = ['csv']
+
+with open('daily.json', 'rb') as f:
+	data = f.readlines()
+
+data = json.loads(data[0])
+lst = []
+for key, value in data.items():
+	lst.append([key.replace("-", ","), value])
+
+with open('hourly.json', 'rb') as f:
+	h_data = f.readlines()
+
+h_data = json.loads(h_data[0])
+
+with open('201601.json', 'rb') as f:
+	jan16_data = f.readlines()
+
+jan16_data = json.loads(jan16_data[0])
+
+with open('201602.json', 'rb') as f:
+	feb16_data = f.readlines()
+
+feb16_data = json.loads(feb16_data[0])
+
+r2_jan16 = r2_score(jan16_data['y_true'], jan16_data['y_pred'])
+
+@app.route('/_get_h_data')
+def get_h_data():
+	"""Add two numbers server side, ridiculous but well..."""
+	year_month = request.args.get('a', type=str)
+	year_month = year_month.split('-')
+	year = int(year_month[0])
+	month = int(year_month[1])
+	tmp_m = month
+	tmp_y = year
+	# my_cal = calendar.Calendar()
+	first = date(year, month, 1)
+	m_lst = []
+	while  month <  tmp_m + 1 and year < tmp_y + 1:
+		m_lst += [first]
+		first += timedelta(days=1)
+		month = first.month
+		year = first.year
+	# for i in my_cal().monthdatescalendar(int(my_cal[0]), int(my_cal[1])):
+	# 	m_lst += map(lambda x: str(x), i)
+	m_lst = sorted(map(lambda x: str(x), m_lst))
+	n = len(m_lst)
+	y = range(24)
+	t = []
+	for i in range(24):
+		t += [map(lambda x: [x], range(n))]
+	tt = []
+	for j in range(n):
+		tt += [map(lambda x: t[x][j] + [x], range(24))]
+	ttt = []
+	for k in range(n):
+		ttt += map(lambda x, y: x + [y], tt[k], h_data[m_lst[k]])
+	return jsonify(day_lst=m_lst, data_lst=ttt, y=y)
+
+@app.route('/_get_d_data')
+def get_d_data():
+	"""Add two numbers server side, ridiculous but well..."""
+	start_date = datetime.strptime((request.args.get('a', type=str)), '%Y-%m-%d').date()
+	end_date = datetime.strptime((request.args.get('b', type=str)), '%Y-%m-%d').date()
+	dict = {}
+	while start_date < end_date:
+		dict[str(start_date)] = h_data[str(start_date)]
+		start_date += timedelta(days=1)
+	dict[str(end_date)] = h_data[str(end_date)]
+	return jsonify(dict=dict)
+
+actual_lst = []
+r2_feb16 = 0
+@app.route('/upload', methods=['POST'])
+def upload():
+	if request.method == 'POST':
+		f = request.files['file_source']
+		print "===================================="
+		if not f:
+			return redirect(url_for('prediction'))
+		now = datetime.now()
+		filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), f.filename.rsplit('.', 1)[1]))
+		f.save(filename)
+		tmp_actual_lst = []
+		with open(filename, 'rb') as csvfile:
+			csvreader = csv.DictReader(csvfile)
+			for i in csvreader:
+				tmp_actual_lst += [int(i['occupancy'])]
+		global actual_lst
+		actual_lst = tmp_actual_lst
+		if len(actual_lst) == len(feb16_data['y_pred']):
+			global r2_feb16 
+			r2_feb16 = r2_score(actual_lst, feb16_data['y_pred'])
+		print r2_feb16
+		return render_template('prediction.html', 
+			jan16_data=jan16_data, feb16_data=feb16_data,
+			tmp_actual_lst=tmp_actual_lst, actual_lst=actual_lst, 
+			r2_jan16=r2_jan16, r2_feb16=r2_feb16)
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/calendar')
+def calendar():
+	return render_template('calendar.html', lst=lst)
+
+@app.route('/heatmap')
+def heatmap():
+	return render_template('heatmap.html')
+
+@app.route('/linechart')
+def linechart():
+	return render_template('linechart.html')
+
+@app.route('/prediction')
+def prediction():
+	return render_template('prediction.html',
+		jan16_data=jan16_data, feb16_data=feb16_data, actual_lst=actual_lst,
+		r2_jan16=r2_jan16, r2_feb16=r2_feb16)
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+if __name__ == "__main__":
+	# app.secret_key = 'super secret key'
+	# app.config['SESSION_TYPE'] = 'filesystem'
+	#app.run(debug=True, host='0.0.0.0', port=5000)
+	app.run(debug=True, port=5000)
